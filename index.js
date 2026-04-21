b881f05cf661a7738cd437cbe84add7d343a1dc5
sunandgmohan <sunandgmohan@gmail.com>
2026-04-21 16:42:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("backend app deployed successfully");
+  res.send("app deployed successfully");
 });
 
 const PORT = 3000;
